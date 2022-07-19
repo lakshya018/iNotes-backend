a516ad8a3d0f62d6567a63d4ce43eafa7a620063
Lakshya Soni <lakshyasoni018@gmail.com>
2022-07-19 14:33:17 +0530

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.get("/",(req,res)=>{
-  res.json("backend start");
+  res.json("server start");
 })
 
 app.listen(port, () => {
